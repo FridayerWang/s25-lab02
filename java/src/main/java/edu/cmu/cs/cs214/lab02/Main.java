ebbf5e2a1ecc732bff44c5aaeac552d221337462
FridayerWang <FridayerWang@gmail.com>
2025-01-24 04:33:54 -0500

    Improve JAVA encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape1 = new Rectangle(2, 3);
+        Shape shape2 = new Square(4);
+        Shape shape3 = new Circle(1);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(shape1);
+        Renderer renderer2 = new Renderer(shape2);
+        Renderer renderer3 = new Renderer(shape3);
 
-        renderer.draw();
+        renderer1.draw();   // Rectangle
+        renderer2.draw();   // Square
+        renderer3.draw();   // Circle
     }
 }
